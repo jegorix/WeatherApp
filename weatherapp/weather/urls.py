9f20010d208dca0332e09e0cb3859940159837b0
jegorix <0311egor@gmail.com>
2025-04-06 02:16:18 +0300

    add doc and support paths

--- weatherapp/weather/urls.py
+++ weatherapp/weather/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name= 'index'),
     path('info', views.info, name = 'info'),
+    path('doc', views.doc, name='doc'),
+    path('support', views.support, name= 'support'),
 ]
