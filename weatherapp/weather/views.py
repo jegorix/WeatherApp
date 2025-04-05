998633181edf4bf133b2c3817d281058817a4029
jegorix <0311egor@gmail.com>
2025-04-05 19:52:36 +0300

    start weatherapp

---
+++ weatherapp/weather/views.py
@@ -0,0 +1,25 @@
+import requests
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def index(request):
+    appid = '60df5caa0b880580fcc637509dbe92ac'
+    city = 'London'
+
+    url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid={appid}'
+    res = requests.get(url).json()
+
+    city_info = {
+        'city': city,
+        'temp': res["main"]["temp"],
+        'icon': res["weather"][0]["icon"],
+    }
+
+    context = {
+        'info': city_info,
+    }
+
+
+    return render(request, 'weather/index.html')
